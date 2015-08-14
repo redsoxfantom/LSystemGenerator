e744f5c22706f1f3ecbea336d0f76f04e8f18c80
Tom <redsoxfantom@gmail.com>
2015-08-14 18:17:29 -0400

    Initial commit

---
+++ LSystemGenerator/LSystemGen/Generator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSystemGenerator.LSystemGen
+{
+    public class Generator
+    {
+        private Dictionary<string, string> mRules;
+
+        public Generator()
+        {
+            mRules = new Dictionary<string, string>();
+        }
+    }
+}
