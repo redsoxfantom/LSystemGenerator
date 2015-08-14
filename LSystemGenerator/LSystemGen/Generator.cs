09d71b206ad7b773c2670b35208716b87de6cc9e
Tom <redsoxfantom@gmail.com>
2015-08-14 18:23:12 -0400

    Created initial pass at Generator

--- LSystemGenerator/LSystemGen/Generator.cs
+++ LSystemGenerator/LSystemGen/Generator.cs
@@ -8,11 +8,41 @@ namespace LSystemGenerator.LSystemGen
 {
     public class Generator
     {
-        private Dictionary<string, string> mRules;
+        private Dictionary<char, string> mRules;
 
         public Generator()
         {
-            mRules = new Dictionary<string, string>();
+            mRules = new Dictionary<char, string>();
+        }
+        
+        public void AddRule(char ruleName, string ruleOutput)
+        {
+            mRules.Add(ruleName, ruleOutput);
+        }
+
+        public string GenerateSystem(int numIters, string input)
+        {
+            StringBuilder output = new StringBuilder(input);
+
+            for(int i = 0; i < numIters; i++)
+            {
+                string iterationInput = output.ToString();
+                output = new StringBuilder();
+
+                foreach(char elem in iterationInput)
+                {
+                    if(mRules.ContainsKey(elem))
+                    {
+                        output.Append(mRules[elem]);
+                    }
+                    else
+                    {
+                        output.Append(elem);
+                    }
+                }
+            }
+
+            return output.ToString();
         }
     }
 }
